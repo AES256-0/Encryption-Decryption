8cac456b5a1357a1b91aee5785d63b24855e1b7e
root <root@localhost.localdomain>
2019-08-26 12:23:02 +0530

    Encryption and Decryption code module

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -17,14 +17,15 @@ def aesccm_mode_encryption(bitlength,filename,key):
     else:
         fd=shelve.open(key)
         key=fd["key"]
-    key1=AESCCM(key[0])
+        key=key[0]
+    key1=AESCCM(key)
     nonce=os.urandom(12)
 
     print("***Encrypting your data***")
     ct=key1.encrypt(nonce,data,None)
     ls=[key,nonce]
-    print("***Saving your data to file:"+filename+"_decrypted.db***")
-    new_name=filename+"_decrypted"
+    print("***Saving your data to file:"+filename+"_encrypted.db***")
+    new_name=filename+"_encrypted"
     db=shelve.open(new_name)
     db["data"]=ct
     db.close()
@@ -44,11 +45,62 @@ def aesccm_mode_decryption(filename,key):
     print("***Reading your keys***")
     fd=shelve.open(key)
     ls=fd["key"]
+    print("***Decrypting the file***")
     data=AESCCM(ls[0]).decrypt(ls[1],fs,None)
     new_file=filename + "_decrypted"
     fd=open(new_file,"w")
     fd.write(str(data))
     fd.close()
+    print("***Decrypted data is saved to file:"+new_file+"***")
+
+
+def aesgcm_mode_encryption(bitlength,filename,key):
+    fd=open(filename,"r")
+    data=fd.read()
+    data=data.encode()
+    fd.close()
+
+    if not key:
+        print("****Generating key with key length:"+str(bitlength))
+        key=AESGCM.generate_key(bit_length=bitlength)
+    else:
+        fd=shelve.open(key)
+        key=fd["key"]
+        key=key[0]
+    key1=AESGCM(key)
+    nonce=os.urandom(12)
+
+    print("***Encrypting your data***")
+    ct=key1.encrypt(nonce,data,None)
+    ls=[key,nonce]
+    print("***Saving your data to file:"+filename+"_encrypted.db***")
+    new_name=filename+"_encrypted"
+    db=shelve.open(new_name)
+    db["data"]=ct
+    db.close()
+    print("***saving you key and nonces to file:"+filename+"_keys.db")
+    key_file=filename+"_keys"
+    db=shelve.open(key_file)
+    db["key"]=ls
+    db.close()
+
+
+def aesgcm_mode_decryption(filename,key):
+    fd=shelve.open(filename)
+    fs=fd["data"]
+    fd.close()
+    print("***Reading your keys***")
+    fd=shelve.open(key)
+    ls=fd["key"]
+    print("***Decrypting the file***")
+    data=AESGCM(ls[0]).decrypt(ls[1],fs,None)
+    new_file=filename + "_decrypted"
+    fd=open(new_file,"w")
+    fd.write(str(data))
+    fd.close()
+    print("***Decrypted data is saved to file:"+new_file+"***")
+
+
 
 if __name__=="__main__":
     pass
